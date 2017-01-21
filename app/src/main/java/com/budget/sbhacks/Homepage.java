3eafd60416a7d57a09be7ecf8dd9841ff02ec0b1
Shreyas Niradi <shreyas.niradi@gmail.com>
2017-01-20 21:03:58 -0800

    Login page commit

---
+++ app/src/main/java/com/budget/sbhacks/Homepage.java
@@ -0,0 +1,19 @@
+package com.budget.sbhacks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Shreyas Niradi on 1/20/2017.
+ * hoh
+ */
+
+public class Homepage extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
