5a59aca1d320ca99493b963decfde9b56168335d
Ekta Shahani <firebolt7@gmail.com>
2017-01-21 12:03:08 -0800

    map view

--- app/src/main/java/com/budget/sbhacks/Homepage.java
+++ app/src/main/java/com/budget/sbhacks/Homepage.java
@@ -12,8 +12,7 @@ public class Homepage extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
     }
-
-
 
 }
